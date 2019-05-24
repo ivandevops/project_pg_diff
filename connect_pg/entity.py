cf0e6a432b2ce7977bc710368a3e096018f66c1d
85493 <lixin@firedata.cc>
2019-05-24 10:12:33 +0800

    update

--- connect_pg/entity.py
+++ connect_pg/entity.py
@@ -8,31 +8,24 @@ class TableColumn:
         self.is_nullable = is_nullable
         self.default = default
 
-    # @property
     def name(self):
         return self.name
 
-    # @property
     def type(self):
         return self.type
 
-    # @property
     def varchar_len(self):
         return self.varchar_len
-
-    # @property
+    
     def double_len(self):
         return self.double_len
 
-    # @property
     def collate(self):
         return self.collate
-
-    # @property
+    
     def is_nullable(self):
         return self.is_nullable
 
-    # @property
     def default(self):
         return self.default
 
@@ -45,23 +38,18 @@ class TableConstraint:
         self.pk = pk
         self.fk = fk
 
-    @property
     def name(self):
         return self.name
-
-    @property
+    
     def ck(self):
         return self.ck
 
-    @property
     def uk(self):
         return self.uk
 
-    @property
     def pk(self):
         return self.pk
-
-    @property
+    
     def fk(self):
         return self.fk
 
@@ -70,12 +58,10 @@ class TableRemark:
     def __init__(self, name=None, desc=None):
         self.name = name
         self.desc = desc
-
-    @property
+    
     def name(self):
         return self.name
 
-    @property
     def desc(self):
         return self.desc
 
